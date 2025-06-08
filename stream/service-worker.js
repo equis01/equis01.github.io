d9f901abb6d0d576575a17302731ab95fb735b68
equis01 <71985026+equis01@users.noreply.github.com>
2025-06-08 17:47:20 -0600

    Update service-worker.js

--- stream/service-worker.js
+++ stream/service-worker.js
@@ -1,10 +1,20 @@
-
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(caches.open('mi-catalogo-v7').then(cache =>
-    cache.addAll(['./', './index.html', './manifest.json'])
-  ));
+  e.waitUntil(
+    caches.open('mi-catalogo-v1').then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './detalle.html',
+        './manifest.json',
+        './service-worker.js'
+      ])
+    )
+  );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
